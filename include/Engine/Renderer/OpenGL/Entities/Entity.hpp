0454ae6134bbea51eb32fb1db82117d9ff531ec8
wyrexdev <uchihasasukedev@gmail.com>
2025-09-21 16:22:57 +0300

    Implement Entity class methods for position, rotation, and scale management

--- include/Engine/Renderer/OpenGL/Entities/Entity.hpp
+++ include/Engine/Renderer/OpenGL/Entities/Entity.hpp
@@ -2,36 +2,28 @@
 
 class Entity {
 public:
-    void setPosition(glm::vec3 pos) {
-        this->pos = pos;
-    }
+    void setPosition(glm::vec3 pos);
+    void setPosition(float x, float y, float z);
+    void setPosition(float x, float y);
+    void setPosX(float x);
+    void setPosY(float y);
+    void setZIndex(float z);
 
-    void setPosition(float x, float y, float z) {
-        this->pos.x = x;
-        this->pos.y = y;
-        this->pos.z = z;
-    }
+    void setRotate(glm::vec3 rotate);
+    void setRotate(float x, float y, float z);
+    void setRotate(float x, float y);
+    void setRotX(float x);
+    void setRotY(float y);
+    void setRotZ(float z);
 
-    void setPosition(float x, float y) {
-        this->pos.x = x;
-        this->pos.y = y;
-    }
+    void setScale(glm::vec3 scale);
+    void setWidth(float x);
+    void setHeight(float y);
+    void setScaleZ(float z);
 
-    void setPosX(float x) {
-        this->pos.x = x;
-    }
-
-    void setPosY(float y) {
-        this->pos.y = y;
-    }
-
-    void setZIndex(float z) {
-        this->pos.z = z;
-    }
-
-    void setRotate(glm::vec3 rotate) {
-
-    }
+    glm::vec3 getPosition();
+    glm::vec3 getRotation();
+    glm::vec3 getScale();
 private:
     glm::vec3 pos;
     glm::vec3 rotate;
---
+++ src/Engine/Renderer/OpenGL/Entities/Entity.cpp
@@ -0,0 +1,83 @@
+#include "Engine/Renderer/OpenGL/Entities/Entity.hpp"
+
+void Entity::setPosition(glm::vec3 pos)
+{
+    this->pos = pos;
+}
+
+void Entity::setPosition(float x, float y, float z)
+{
+    this->pos.x = x;
+    this->pos.y = y;
+    this->pos.z = z;
+}
+
+void Entity::setPosition(float x, float y)
+{
+    this->pos.x = x;
+    this->pos.y = y;
+}
+
+void Entity::setPosX(float x)
+{
+    this->pos.x = x;
+}
+
+void Entity::setPosY(float y)
+{
+    this->pos.y = y;
+}
+
+void Entity::setZIndex(float z)
+{
+    this->pos.z = z;
+}
+
+
+
+void Entity::setRotate(glm::vec3 rotate)
+{
+    this->rotate = rotate;
+}
+
+void Entity::setRotate(float x, float y, float z) {
+    this->rotate.x = x;
+    this->rotate.y = y;
+    this->rotate.z = z;
+}
+
+
+void Entity::setRotate(float x, float y) {
+    this->rotate.x = x;
+    this->rotate.y = y;
+}
+
+void Entity::setRotX(float x) {
+    this->rotate.x = x;
+}
+
+void Entity::setRotY(float y) {
+    this->rotate.y = y;
+}
+
+void Entity::setRotZ(float z) {
+    this->rotate.z = z;
+}
+
+
+
+void Entity::setScale(glm::vec3 scale) {
+    this->scale = scale;
+}
+
+void Entity::setWidth(float w) {
+    this->scale.x = w;
+}
+
+void Entity::setHeight(float h) {
+    this->scale.y = h;
+}
+
+void Entity::setScaleZ(float z) {
+    this->scale.z = z;
+}
