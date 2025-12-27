df8d1cee4c64eced30cc274107599dff9612e510
Wyrex <uchihasasukedev@gmail.com>
2025-12-27 20:19:37 +0300

    Refactor: Enhance Client class with error handling in initClient and complete sendRequest implementation

--- include/Core/IPC/Client.hpp
+++ include/Core/IPC/Client.hpp
@@ -1,8 +1,22 @@
 #pragma once
 
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <cstring>
+#include <iostream>
+
+#include "Core/IPC/Protocol.hpp"
+
 namespace IPC
 {
-    class Client {
-        
+    class Client
+    {
+    public:
+        void initClient();
+        Protocol::Response sendRequest(Protocol::Request req);
+
+    private:
+        int sock;
     };
 } // namespace IPC
--- src/Core/IPC/Client.cpp
+++ src/Core/IPC/Client.cpp
@@ -2,5 +2,44 @@
 
 namespace IPC
 {
-    
+    void Client::initClient()
+    {
+        int sock = socket(AF_UNIX, SOCK_STREAM, 0);
+        if (sock < 0)
+        {
+            perror("socket");
+        }
+
+        sockaddr_un addr{};
+        addr.sun_family = AF_UNIX;
+        strcpy(addr.sun_path, "/var/run/void.sock");
+
+        if (connect(sock, (sockaddr *)&addr, sizeof(addr)) < 0)
+        {
+            perror("connect");
+        }
+    }
+
+    Protocol::Response Client::sendRequest(Protocol::Request req)
+    {
+        Protocol::Request req{};
+        req.magic = 0xDEADBEEF;
+        req.cmd = Command::WRITE_FILE;
+        strcpy(req.path, "hello.txt");
+
+        const char msg[] = "Merhaba Void Sandbox";
+        req.data_len = sizeof(msg);
+        memcpy(req.data, msg, req.data_len);
+
+        write(sock, &req, sizeof(req));
+
+        Protocol::Response res{};
+        read(sock, &res, sizeof(res));
+
+        std::cout << "Server status: " << res.status << std::endl;
+
+        close(sock);
+
+        return res;
+    }
 } // namespace IPC
