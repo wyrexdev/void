31856c31668faa242f6459f40ae9f1c7997ef378
Wyrex <uchihasasukedev@gmail.com>
2025-12-27 17:04:59 +0300

    Refactor: Implement sandbox method in SandboxMain class and update readFile method in FileStore class

--- include/Core/Sandbox/SandboxMain.hpp
+++ include/Core/Sandbox/SandboxMain.hpp
@@ -16,34 +16,6 @@ namespace IPC
     class SandboxMain
     {
     public:
-        void sandboxMain()
-        {
-            int server = socket(AF_UNIX, SOCK_STREAM, 0);
-
-            sockaddr_un addr{};
-            addr.sun_family = AF_UNIX;
-            strcpy(addr.sun_path, "/var/run/void.sock");
-
-            unlink(addr.sun_path);
-            bind(server, (sockaddr *)&addr, sizeof(addr));
-            listen(server, 5);
-
-            while (true)
-            {
-                int client = accept(server, nullptr, nullptr);
-
-                Protocol::Request req;
-                read(client, &req, sizeof(req));
-
-                Protocol::Response res{};
-                if (req.cmd == Command::READ_FILE)
-                {
-                    res.status = FileStore::readFile(req.path, res.data, &res.data_len);
-                }
-
-                write(client, &res, sizeof(res));
-                close(client);
-            }
-        }
+        void sandbox();
     };
 } // namespace IPC
--- src/Core/Storage/FileStore.cpp
+++ src/Core/Storage/FileStore.cpp
@@ -2,5 +2,55 @@
 
 namespace IPC
 {
-    
+    int FileStore::readFile(
+        const char *rel_path,
+        uint8_t *out_data,
+        uint32_t *out_len)
+    {
+        if (strstr(rel_path, "..") || rel_path[0] == '/')
+        {
+            return -EACCES;
+        }
+
+        char full_path[512];
+        snprintf(full_path, sizeof(full_path),
+                 "%s/%s", SANDBOX_ROOT, rel_path);
+
+        int fd = open(full_path, O_RDONLY | O_NOFOLLOW);
+        if (fd < 0)
+        {
+            return -errno;
+        }
+
+        struct stat st;
+        if (fstat(fd, &st) != 0)
+        {
+            close(fd);
+            return -errno;
+        }
+
+        if (!S_ISREG(st.st_mode))
+        {
+            close(fd);
+            return -EACCES;
+        }
+
+        if (st.st_size > MAX_DATA_LEN)
+        {
+            close(fd);
+            return -EFBIG;
+        }
+
+        ssize_t r = read(fd, out_data, st.st_size);
+        if (r < 0)
+        {
+            close(fd);
+            return -errno;
+        }
+
+        *out_len = static_cast<uint32_t>(r);
+
+        close(fd);
+        return 0;
+    }
 } // namespace IPC
