183d3d7b07db9c04b5fa469b05adb6b9eabe0732
Wyrex <uchihasasukedev@gmail.com>
2026-01-07 22:55:05 +0300

    Update EditTextRenderer to adjust text positioning and enhance visual elements. Change text color to white, modify rectangle dimensions, and add a pointer indicator for improved user interaction.

--- src/Skia/Elements/Inputs/Text/EditTextRenderer.cpp
+++ src/Skia/Elements/Inputs/Text/EditTextRenderer.cpp
@@ -11,25 +11,35 @@ namespace Skia
     void EditTextRenderer::onInit()
     {
         text = new TextRenderer(canvas, parentWidget);
-        text->setTextColor(255, 255, 0, 0);
+        text->setTextColor(255, 255, 255, 255);
         text->setWeight(200);
     }
 
     void EditTextRenderer::onRender()
     {
-        text->setX(getX());
-        text->setY(getY());
+        text->setX(getX() + 10);
+        text->setY(getY() + 10);
 
         text->init();
 
+        SkPaint pointerPaint;
+        pointerPaint.setAntiAlias(true);
+        pointerPaint.setColor(SkColorSetRGB(200, 200, 200));
+
         SkPaint rectPaint;
         rectPaint.setAntiAlias(true);
-        rectPaint.setColor(SkColorSetRGB(17, 255, 17));
+        rectPaint.setStyle(SkPaint::kStroke_Style);
+        rectPaint.setStrokeWidth(2.0f);
+        rectPaint.setColor(SkColorSetRGB(200, 200, 200));
 
         canvas->drawRoundRect(
-            SkRect::MakeXYWH(getX(), getY(), text->getWidth(), text->getHeight() * 2),
-            20, 20,
+            SkRect::MakeXYWH(getX(), getY(), text->getWidth() + 30, (text->getHeight() * 2) + 20),
+            0, 0,
             rectPaint);
+        canvas->drawRoundRect(
+            SkRect::MakeXYWH(getX() + text->getWidth() + 25, getY() + 10, 2, (text->getHeight() * 2)),
+            0, 0,
+            pointerPaint);
         text->render();
     }
 
